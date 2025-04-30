eb5c6373505ab2bd83012840790c50e872c54df3
Bijo Murmu <bijomurmu036@gmail.com>
2025-04-30 20:08:45 +0530

    Updates

---
+++ precache-manifest.8967060a40a96ff25ad785a931aa082f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d869aa7dbc2d7fc73e8890e8681c16",
+    "url": "/BlockBallot/index.html"
+  },
+  {
+    "revision": "ce9af6984a35e563dce6",
+    "url": "/BlockBallot/static/css/main.5d6feff6.chunk.css"
+  },
+  {
+    "revision": "774bf638b7838a59f070",
+    "url": "/BlockBallot/static/js/2.e9495ac4.chunk.js"
+  },
+  {
+    "revision": "ce9af6984a35e563dce6",
+    "url": "/BlockBallot/static/js/main.11ed1507.chunk.js"
+  },
+  {
+    "revision": "f7af84797e291323e57c",
+    "url": "/BlockBallot/static/js/runtime-main.0a2d0a27.js"
+  }
+]);
